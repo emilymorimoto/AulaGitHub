700a91a0fd5af29e952a9ba627aae98819dcc848
Emily <morimoto.emily@Icloud.com>
2023-06-29 16:19:16 -0300

    Initial Commit

---
+++ AulaGitHub/AulaGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  AulaGitHubApp.swift
+//  AulaGitHub
+//
+//  Created by Emily Morimoto on 29/06/23.
+//
+
+import SwiftUI
+
+@main
+struct AulaGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
